b488e2ff43ed03d36f2396c20b75aa457033ad1e
BernasPL3 <bernardogamesguidi@gmail.com>
2026-06-17 13:01:55 -0300

    Create SpriteRendererComponent.h

---
+++ Engine/Scene/SpriteRendererComponent.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Nebula
+{
+    struct SpriteRendererComponent
+    {
+        float R = 1.0f;
+        float G = 1.0f;
+        float B = 1.0f;
+        float A = 1.0f;
+    };
+}
