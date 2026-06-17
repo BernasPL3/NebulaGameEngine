fe2a7c62ac7bf497131c1460c73433ba283d6f74
BernasPL3 <bernardogamesguidi@gmail.com>
2026-06-17 12:51:26 -0300

    Create Renderer.h

---
+++ Engine/Renderer/Renderer.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace Nebula
+{
+    class Renderer
+    {
+    public:
+        static void Init();
+        static void BeginFrame();
+        static void EndFrame();
+    };
+}
