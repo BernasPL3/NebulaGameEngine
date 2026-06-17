6a8ae70aaef42cbe9e2df78935cefc0235cc2428
BernasPL3 <bernardogamesguidi@gmail.com>
2026-06-17 13:17:57 -0300

    Create Collider2D.h

---
+++ Engine/Physics/Collider2D.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace Nebula
+{
+    class Collider2D
+    {
+    public:
+        float Width = 1.0f;
+        float Height = 1.0f;
+
+        bool IsTrigger = false;
+    };
+}
