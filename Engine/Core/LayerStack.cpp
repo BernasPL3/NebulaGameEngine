8762d87b4b4c97b21dcb609c7ae5936c2a11dcc9
BernasPL3 <bernardogamesguidi@gmail.com>
2026-06-17 12:47:45 -0300

    Create LayerStack.cpp

---
+++ Engine/Core/LayerStack.cpp
@@ -0,0 +1,26 @@
+#include "LayerStack.h"
+#include <algorithm>
+
+namespace Nebula
+{
+    void LayerStack::PushLayer(Layer* layer)
+    {
+        m_Layers.emplace_back(layer);
+        layer->OnAttach();
+    }
+
+    void LayerStack::PopLayer(Layer* layer)
+    {
+        auto it = std::find(
+            m_Layers.begin(),
+            m_Layers.end(),
+            layer
+        );
+
+        if (it != m_Layers.end())
+        {
+            layer->OnDetach();
+            m_Layers.erase(it);
+        }
+    }
+}
