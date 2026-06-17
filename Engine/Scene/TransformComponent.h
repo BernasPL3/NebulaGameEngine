febb37445503680547931702c070fca77a36af9f
BernasPL3 <bernardogamesguidi@gmail.com>
2026-06-17 13:01:08 -0300

    Create TransformComponent.h

---
+++ Engine/Scene/TransformComponent.h
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Nebula
+{
+    struct TransformComponent
+    {
+        float PositionX = 0.0f;
+        float PositionY = 0.0f;
+        float PositionZ = 0.0f;
+
+        float RotationX = 0.0f;
+        float RotationY = 0.0f;
+        float RotationZ = 0.0f;
+
+        float ScaleX = 1.0f;
+        float ScaleY = 1.0f;
+        float ScaleZ = 1.0f;
+    };
+}
