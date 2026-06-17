0cf91ef4811a126804004bf99b6e0f7c9768db2b
BernasPL3 <bernardogamesguidi@gmail.com>
2026-06-17 12:48:09 -0300

    Create Log.h

---
+++ Engine/Core/Log.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+
+#define NB_INFO(x)  std::cout << "[INFO] " << x << std::endl
+#define NB_WARN(x)  std::cout << "[WARN] " << x << std::endl
+#define NB_ERROR(x) std::cout << "[ERROR] " << x << std::endl
