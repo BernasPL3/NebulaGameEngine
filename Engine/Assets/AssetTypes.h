cee0d7333ccae2ac4bfbcfc3eaf5465d6a42d3e7
BernasPL3 <bernardogamesguidi@gmail.com>
2026-06-17 13:10:25 -0300

    Create AssetTypes.h

---
+++ Engine/Assets/AssetTypes.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Nebula
+{
+    enum class AssetType
+    {
+        None = 0,
+        Texture2D,
+        Model3D,
+        Audio,
+        Shader,
+        Scene
+    };
+}
